b5427f1488eaade353ad4c39a4904a9bc1f5284d
David <davidostos2@gmail.com>
2023-03-17 16:49:53 -0400

    clase 1 routing

--- my-store/.eslintrc.js
+++ my-store/.eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     "ecmaVersion": "2018"
   },
   "rules": {
-    "no-console": "warn",
     "indent": [
       "error",
       2
--- my-store/index.js
+++ my-store/index.js
@@ -1,1 +1,23 @@
-console.log("My app");
+const express = require("express");
+const app = express();
+const port = 3000;
+const IP = "192.168.100.211";
+
+app.get("/", (req,res) => {
+  res.send("Hola mi server en express");
+});
+
+app.get("/nueva-ruta", (req,res) => {
+  res.send("Hola soy una nueva ruta");
+});
+
+app.get("/products", (req,res) => {
+  res.json({
+    name: "producto 1",
+    price: 1000
+  });
+});
+
+app.listen(port, () => {
+  console.log("http://"+ IP +":" + port + "/");
+});

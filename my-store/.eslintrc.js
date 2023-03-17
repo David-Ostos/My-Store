0866fb223c192d8fac647797be2494c509c72cd2
David <davidostos2@gmail.com>
2023-03-17 14:16:47 -0400

    preparando el espaio de trabajo

---
+++ my-store/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jest": true
+  },
+  "extends": ["eslint:recommended",
+    "prettier"],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "2018"
+  },
+  "rules": {
+    "no-console": "warn",
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};

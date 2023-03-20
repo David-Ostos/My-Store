3df81c43e0d025e9edba1d32a86b732e75ff42f8
David <davidostos2@gmail.com>
2023-03-20 18:35:39 -0400

    clase 7 validacion con joi

---
+++ my-store/middleware/validator.handler.js
@@ -0,0 +1,25 @@
+const boom  = require('@hapi/boom');
+
+
+function validatorHandler( schema, property ){         // con esta funcion hay creamos un middleware dinamico, para poder saber si es de tipo post "body" get "params o querys"
+
+  return ( req, res, next ) => {                    // se devuelve un middleware
+
+    const data = req[property];                   // para validar la data y saber de que tipo es
+    const { error } = schema.validate(data);    // para valuidar el tipo de error
+
+    if(error){                                // si es un error, envia el boom para tratarlo en us middleware correspondiente
+      next(boom.badRequest(error));
+
+    }else{                                   // si no continua tranquilamente
+      next();
+
+    }
+  };
+}
+
+
+
+module.exports = validatorHandler;
+
+

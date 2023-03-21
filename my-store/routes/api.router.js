53f3a986e416b1a072e4ee4e0b85ed89c9d7e85d
David <davidostos2@gmail.com>
2023-03-20 22:01:41 -0400

    cambiando nombre de carpeta

--- my-store/routes/api.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/api', (req,res) => {                  // asi se crea una nueva page donde res(response) es la info que se trae del servidor y req(require) es la informacion que se manda al servidor
-  res.send('Hola mi server en express');    //  se utilisa res.send() para enviar algo a la url designada
-});
-
-module.exports = router;

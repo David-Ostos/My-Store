c031c1fb753e730548e8df5fb46298d8e327a7d0
David <davidostos2@gmail.com>
2023-03-21 16:19:39 -0400

    clase 8 cors ejemplos de como manejarlo con libreria cors

--- my-store_server/index.js
+++ my-store_server/index.js
@@ -3,12 +3,25 @@
 const express = require('express');
 const routerApi = require('./routes');
 const { errorHandler, logErrors, boomErrorHandler }  = require('./middleware/error.handler');
+const cors = require('cors');
+
+const whitelist = ['http://127.0.0.1:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)){
+      callback(null, true);
+    }else{
+      callback(new Error('no permitido'));
+    }
+  }
+};
 
 const app = express();              // aqui se cran las constantes para poder activar express
 const port = 3000;                // constante que tiene el port que vamos a utilizar
 const IP = '192.168.100.211';   // const donde tenemos la IP de nuestro router para poder ver la pagina en todos los dispositivos
 
 app.use(express.json());
+app.use(cors(options));
 
 /* app.get('/', (req,res) => {                  // asi se crea una nueva page donde res(response) es la info que se trae del servidor y req(require) es la informacion que se manda al servidor
   res.send('Hola mi server en express');    //  se utilisa res.send() para enviar algo a la url designada

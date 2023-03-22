93d246851f5286504398bcbf41273d5742a39079
David <davidostos2@gmail.com>
2023-03-22 11:08:55 -0400

    actualizando para hacer  delploy en vercel

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const whitelist = ['https://my-app-express-krr9.onrender.com',
 
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1){
+    if (whitelist.includes(origin)) {
       callback(null, true);
     }else{
       callback(new Error('no permitido, cors no acceptado'));
@@ -19,19 +19,19 @@ const options = {
   }
 };
 
-const app = express();              // aqui se cran las constantes para poder activar express
-const port = process.env.PORT ||3000;                // constante que tiene el port que vamos a utilizar
-const IP = '192.168.100.211';   // const donde tenemos la IP de nuestro router para poder ver la pagina en todos los dispositivos
+const app = express();                            // aqui se cran las constantes para poder activar express
+const port = process.env.PORT ||3000;           // constante que tiene el port que vamos a utilizar, es recomendable establecer que lo maneje una variable de entorno
+const IP = '192.168.100.211';                 // const donde tenemos la IP de nuestro router para poder ver la pagina en todos los dispositivos
 
 app.use(express.json());
 
-/* app.get('/', (req,res) => {                  // asi se crea una nueva page donde res(response) es la info que se trae del servidor y req(require) es la informacion que se manda al servidor
+app.get('/api', (req,res) => {                  // asi se crea una nueva page donde res(response) es la info que se trae del servidor y req(require) es la informacion que se manda al servidor
   res.send('Hola mi server en express');    //  se utilisa res.send() para enviar algo a la url designada
 });
 
-app.get('/nueva-ruta', (req,res) => {   //creando nueva ruta
+app.get('/api/nueva-ruta', (req,res) => {   //creando nueva ruta
   res.send('Hola soy una nueva ruta');
-}); */
+});
 
 routerApi(app);
 
@@ -41,9 +41,6 @@ app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.get('/', (req, res) =>{
-  res.status(200).send({ msg: 'Hola Mundo'});
-});
 
 app.listen(port, () => {
   console.log('http://'+ IP +':' + port + '/');
